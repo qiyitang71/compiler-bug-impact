3087c835aec8b7111bcd840e51d85cdcac92874d
user42 <user42@debian>
2019-07-04 18:41:21 -0400

    ignore data/results folder and remove artifact folder

--- artifact/cloud-scripts/config.rb
+++
@@ -1,8 +0,0 @@
-
-MASTERNODE_CONFIG = {
-  # modes that don't have an executable file
-  :pseudo_modes => ["binary-only", "parallel", 'arch-all-only', 'binarch-only'],
-
-  # run sbuild with this user
-  :sbuild_user => "user42"
-}
