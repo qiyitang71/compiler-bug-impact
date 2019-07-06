fb894818adc1e0b09e4290825fc01c3d7a0f520b
qt211 <qiyi.tang11@imperial.ac.uk>
2019-07-06 17:41:31 +0100

    add scripts to download and compile the compilers

---
+++ scripts/compilers/build-compiler.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "No arguments supplied. Usage is ./build-compiler.sh \$bug_id "
+    exit 1
+fi
+bug_id=$1
+working_folder=/home/user42/$bug_id
+
+if [ ! -d "$working_folder" ]; then
+  echo "$working_folder not exist. Download the source code for bug $bug_id first "
+  exit 1
+fi
+
+sudo chown user42:sbuild $working_folder
+cd $working_folder
+
+if ! [ -e "patch.txt" ]
+  then
+   echo "No patch.txt file found. Stopping here."
+   exit 1
+fi
+rm ./compilers.log
+
+# COMPILING BUGGY COMPILER
+echo "--> [1/3] "$(date | awk '{print $4}')" - BUILDING BUGGY COMPILER "
+cd $working_folder/buggy
+mkdir llvm-build
+rm -rf llvm-install
+mkdir llvm-install
+cd $working_folder/buggy/llvm-build
+# Depending on LLVM version, compilation must be made using either make or cmake: comment the required line to choose between them
+cmake -DCMAKE_BUILD_TYPE="Release" $working_folder/buggy/llvm >>/home/user42/compilers.log 2>&1
+make -j$(nproc) >>/home/user42/compilers.log 2>&1
+cmake -DCMAKE_INSTALL_PREFIX=$working_folder/buggy/llvm-install -P cmake_install.cmake >>/home/user42/compilers.log 2>&1
+#../llvm/configure --prefix=$working_folder/buggy/llvm-install --enable-optimized --disable-compiler-version-checks >>/home/user42/compilers.log 2>&1
+#make -j$(nproc) >>/home/user42/compilers.log 2>&1
+#make install >>/home/user42/compilers.log 2>&1
+
+# COMPILING FIXED COMPILER
+#echo "--> [2/3] "$(date | awk '{print $4}')" - BUILDING FIXED COMPILER "
+cd $working_folder/fixed
+mkdir llvm-build
+mkdir llvm-install
+cd $working_folder/fixed/llvm-build
+
+# Depending on LLVM version, compilation must be made using either make or cmake: comment the required line to choose between them
+cmake -DCMAKE_BUILD_TYPE="Release" $working_folder/fixed/llvm >>/home/user42/compilers.log 2>&1
+make -j$(nproc) >>/home/user42/compilers.log 2>&1
+cmake -DCMAKE_INSTALL_PREFIX=$working_folder/fixed/llvm-install -P cmake_install.cmake >>/home/user42/compilers.log 2>&1
+#../llvm/configure --prefix=$working_folder/fixed/llvm-install --enable-optimized >>/home/user42/compilers.log 2>&1
+#make -j$(nproc) >>/home/user42/compilers.log 2>&1
+#make install >>/home/user42/compilers.log 2>&1
+
+# COMPILING COP COMPILER
+echo "--> [3/3] "$(date | awk '{print $4}')" - BUILDING COP COMPILER "
+cd $working_folder/cop
+patch -p0 < $working_folder/patch.txt
+mkdir llvm-build
+mkdir llvm-install
+cd llvm-build
+
+# Depending on LLVM version, compilation must be made using either make or cmake: comment the required line to choose between them
+cmake -DCMAKE_BUILD_TYPE="Release" $working_folder/cop/llvm >>/home/user42/compilers.log 2>&1
+make -j$(nproc) >>/home/user42/compilers.log 2>&1
+cmake -DCMAKE_INSTALL_PREFIX=$working_folder/cop/llvm-install -P cmake_install.cmake >>/home/user42/compilers.log 2>&1
+#../llvm/configure --prefix=$working_folder/cop/llvm-install --enable-optimized >>/home/user42/compilers.log 2>&1
+#make -j$(nproc) >>/home/user42/compilers.log 2>&1
+#make install >>/home/user42/compilers.log 2>&1
+
+echo ""$(date | awk '{print $4}')" - COMPILERS INSTALL FOR LLVM "$working_folder" DONE"
+
---
+++ scripts/compilers/download-sources.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+  then
+    echo "No arguments supplied. Usage is ./download-sources.sh \$bug_id \$revision "
+    exit 1
+fi
+
+# argument 1 is bug id
+working_folder=/home/user42/$1
+
+#argument 2 is revision number
+revision=$2
+revisionmo=$(( revision - 1 ))
+
+sudo rm -rf $working_folder
+sudo mkdir $working_folder
+sudo chown user42:sbuild $working_folder
+if ! [ -e "patch.txt" ]
+  then
+   echo "No patch.txt file found. Stopping here."
+   exit 1
+fi
+rm ./compilers.log
+
+sudo echo ""$(date | awk '{print $4}')" - STARTING COMPILERS INSTALL FOR LLVMv"$revision" (output to /home/user42/compilers.log)"
+
+# Downloading LLVM sources
+# for clang verison < 3.8.0, compiler-rt is not optional 
+# so please uncomment the code to download compiler-rt
+
+echo "--> [1/4] "$(date | awk '{print $4}')" - DOWLOADING AND PREPARING COMPILER SOURCES "
+cp patch.txt $working_folder
+cd $working_folder
+mkdir buggy
+mkdir fixed
+mkdir cop
+mv patch.txt ./cop
+svn checkout https://llvm.org/svn/llvm-project/llvm/trunk@$revision llvm >>/home/user42/compilers.log 2>&1
+rm -rf ./llvm/.svn
+cd ./llvm/tools
+svn checkout https://llvm.org/svn/llvm-project/cfe/trunk@$revision clang >>/home/user42/compilers.log 2>&1
+rm -rf ./clang/.svn
+cd $working_folder
+#cd llvm/projects
+#svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk@$revision compiler-rt >>/home/user42/compilers.log 2>&1
+#rm -rf ./compiler-rt/.svn
+cd $working_folder
+cp -r ./llvm ./fixed
+mv ./llvm ./cop
+cd buggy
+svn checkout https://llvm.org/svn/llvm-project/llvm/trunk@$revisionmo llvm >>/home/user42/compilers.log 2>&1
+rm -rf ./llvm/.svn
+cd ./llvm/tools
+svn checkout https://llvm.org/svn/llvm-project/cfe/trunk@$revisionmo clang >>/home/user42/compilers.log 2>&1
+rm -rf ./clang/.svn
+cd $working_folder/buggy
+#cd llvm/projects
+#svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk@$revisionmo compiler-rt >>/home/user42/compilers.log 2>&1
+#rm -rf ./compiler-rt/.svn
